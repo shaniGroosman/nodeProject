fcb062935eb1919384ff3ab9d93d0590b598d260
shaniGroosman <6771860@gmaill.com>
2025-03-06 13:13:19 +0200

    save password

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
-import { userModel } from "../models/user.js"
-//שליפת כל המשתמשים 
+import { userModel } from "../models/user.js";
+
+// שליפת כל המשתמשים 
 export const getAllUser = async (req, res) => {
     try {
         let data = await userModel.find().select('-password');
@@ -7,12 +8,11 @@ export const getAllUser = async (req, res) => {
     }
     catch (err) {
         console.log(err);
-        res.status(400).json({ title: "cannot get all user", message: err.message })
-
+        res.status(400).json({ title: "cannot get all user", message: err.message });
     }
-}
-//שליפת משתמש לפי ID
+};
 
+// שליפת משתמש לפי ID
 export const getById = async (req, res) => {
     let { id } = req.params;
     try {
@@ -24,66 +24,74 @@ export const getById = async (req, res) => {
         res.status(400).json({ title: "cannot get by id", message: err.message });
     }
 };
-//הוספת משתמש
+
+// הוספת משתמש
 export const addUser = async (req, res) => {
     let { body } = req;
     if (!body.password || !body.email)
-        return res.status(400).json({ title: "cannot add user", message: "password, email are require" })
+        return res.status(400).json({ title: "cannot add user", message: "password and email are required" });
+
+    if (!(body.password.length >= 6 && (body.password.match(/\d/g) || []).length >= 4 && (body.password.match(/[a-zA-Z]/g) || []).length >= 2))
+        return res.status(400).json({ title: "invalid password", message: "password must be at least 6 characters long, contain at least 4 numbers and 2 letters" });
+
     try {
         let newUser = new userModel(body);
         await newUser.save();
         res.json(newUser);
     }
     catch (err) {
-        console.log(err)
-        res.status(400).json({ title: "cannot add this user", message: err.message })
+        console.log(err);
+        res.status(400).json({ title: "cannot add this user", message: err.message });
     }
-}
+};
 
-//עדכון פרטי משתמש ללא סיסמא
-
+// עדכון פרטי משתמש חוץ מסיסמא
 export const update = async (req, res) => {
     let { id } = req.params;
     let body = req.body;
     if (body.password)
-        return res.status(404).json({ title: "cannot update password", message: "cannot update here password" })
+        return res.status(404).json({ title: "cannot update password", message: "cannot update password here" });
     try {
-        let data = await userModel.findByIdAndUpdate(id, body, { new: true }).select('-password');
+        let data = await userModel.findByIdAndUpdate(id, body, { new: true });
         if (!data) return res.status(404).json({ title: "cannot update by id", message: "user with such id not found" });
         res.json(data);
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(400).json({ title: "cannot update", message: err.message });
     }
-}
-//עדכון  סיסמא
+};
+
+// עדכון סיסמא
 export const updatePassword = async (req, res) => {
     let { id } = req.params;
     let body = req.body;
     if (!body.password || body.userName || body.email)
-      return  res.status(404).json({ title: "only update password", message: "cannot update email userName" })
+        return res.status(400).json({ title: "only update password", message: "cannot update email or userName" });
+
+    if (!(body.password.length >= 6 && (body.password.match(/\d/g) || []).length >= 4 && (body.password.match(/[a-zA-Z]/g) || []).length >= 2))
+        return res.status(400).json({ title: "invalid password", message: "password must be at least 6 characters long, contain at least 4 numbers and 2 letters" });
+
     try {
-        let data = await userModel.findByIdAndUpdate(id, { password: body.password }, { new: true }).select('-password');
+        let data = await userModel.findByIdAndUpdate(id, { password: body.password }, { new: true });
         if (!data) return res.status(404).json({ title: "cannot update by id", message: "user with such id not found" });
         res.json(data);
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(400).json({ title: "cannot update", message: err.message });
     }
-}
+};
 
-
-//כניסה 
+// כניסה 
 export async function getUserByUsernamePassword_Login(req, res) {
     try {
         let data = await userModel.findOne({
-            password: req.body.password,
-            username: req.body.userName
+            username: req.body.userName,
+            password: req.body.password  // שימוש בסיסמה כטקסט רגיל (לא מאובטח!)
         }).select('-password');
 
         if (!data) {
             return res.status(404).json({
-                title: "cannot find user with such details",
+                title: "cannot find user",
                 message: "wrong username or password"
             });
         }
@@ -97,6 +105,3 @@ export async function getUserByUsernamePassword_Login(req, res) {
         });
     }
 }
-
-
-
